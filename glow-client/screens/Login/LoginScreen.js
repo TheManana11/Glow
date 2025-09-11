acf22ed0822743b2fe5250428b588ec42c05eb32
TheManana11 <ahmadmanana815@gmail.com>
2025-09-11 04:22:44 +0300

    fix AM-6: Created login page on frontend

---
+++ glow-client/screens/Login/LoginScreen.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+} from "react-native";
+import styles from "./style";
+import { Feather } from "@expo/vector-icons";
+
+export default function SignupScreen({ navigation }) {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = () => {
+    console.log(formData);
+    setFormData({
+      email: "",
+      password: ""
+    });
+  };
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Image
+        source={require("../../assets/images/logo.png")}
+        style={styles.logo}
+      />
+      <Text style={styles.title}>SkinCare AI</Text>
+      <Text style={styles.subtitle}>Welcome back to your skin journey</Text>
+
+      <View style={styles.form}>
+        <Text style={styles.formTitle}>Login</Text>
+        <Text style={styles.formSubtitle}>
+          Enter you credentials to access your account
+        </Text>
+
+        <Text style={styles.label}>Email</Text>
+        <View style={styles.inputWithIcon}>
+          <Feather name="mail" size={18} color="#D89250" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="e.g. example@gmail.com"
+            value={formData.email}
+            onChangeText={(text) => setFormData({ ...formData, email: text })}
+          />
+        </View>
+
+        <Text style={styles.label}>Password</Text>
+        <View style={styles.inputWithIcon}>
+          <Feather name="lock" size={18} color="#D89250" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="enter your password"
+            secureTextEntry
+            value={formData.password}
+            onChangeText={(text) =>
+              setFormData({ ...formData, password: text })
+            }
+          />
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.footerText}>
+          New to Glow?{"  "}
+          <Text
+            onPress={() => navigation.navigate("Signup")}
+            style={styles.loginLink}
+          >
+            Create an account
+          </Text>
+        </Text>
+      </View>
+    </ScrollView>
+  );
+}
