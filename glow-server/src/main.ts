2b55659497eb15d8037202e3fa2926a116db2e20
TheManana11 <ahmadmanana815@gmail.com>
2025-09-15 02:25:43 +0300

    Fixed request size limit issue

--- glow-server/src/main.ts
+++ glow-server/src/main.ts
@@ -4,9 +4,13 @@ import { join } from 'path';
 import * as express from 'express';
 import { SwaggerModule } from '@nestjs/swagger';
 import { CORS_CONFIG, SWAGGER_CONFIG, VALIDATOR_CONFIG } from './config'
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json({ limit: '20mb' }));
+  app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
 
   app.enableCors(CORS_CONFIG);
   app.useGlobalPipes(VALIDATOR_CONFIG);
