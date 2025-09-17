fdc51b72e6b043e4dde828ed41b5dff979ec26c7
TheManana11 <ahmadmanana815@gmail.com>
2025-09-17 20:49:03 +0300

    Fixed another entity bug, registered it in the license module

--- glow-server/src/license/license.module.ts
+++ glow-server/src/license/license.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LicenseService } from './license.service';
 import { LicenseController } from './license.controller';
+import { License } from './entities/license.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([License])],
   controllers: [LicenseController],
   providers: [LicenseService],
 })
